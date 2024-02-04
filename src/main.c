f0f3feecb131d6dc2dbcd754a21abeee3c6f0bb2
Nutpile ‎ <Nutpile@hotmail.com>
2024-02-04 17:21:43 +0100

    Added time status bar

--- src/main.c
+++ src/main.c
@@ -2,17 +2,38 @@
 #include <graphx.h>
 #include <ti/getcsc.h>
 
+void drawstatusbar()
+{
+    uint8_t day = 0;
+    uint8_t month = 0;
+    uint16_t year = 0;
+    gfx_SetTextXY(5, 4);
+    gfx_SetColor(195);
+    gfx_FillRectangle_NoClip(0, 0, 320, 15);
+    if(!rtc_IsBusy()){
+        gfx_PrintUInt(rtc_Hours, 2);
+        gfx_PrintChar(*":");
+        gfx_PrintUInt(rtc_Minutes, 2);
+        gfx_SetTextXY(235, 4);
+        boot_GetDate(&day, &month, &year);
+        gfx_PrintUInt(day, 2);
+        gfx_PrintChar(*"/");
+        gfx_PrintUInt(month, 2);
+        gfx_PrintChar(*"/");
+        gfx_PrintUInt(year, 4);
+    }
+}
+
 int main()
 {
     gfx_Begin();
-    gfx_SetTextScale(5, 5);
-    gfx_SetTextXY(30, 20);
-    if(!rtc_IsBusy()){
-         gfx_PrintUInt(rtc_Hours, 2);
-         gfx_PrintChar(*":");
-         gfx_PrintUInt(rtc_Minutes, 2);
+    uint8_t barupdate = -1;
+    while(!os_GetCSC()){
+        if (barupdate != rtc_Minutes){
+            drawstatusbar(); 
+            barupdate = rtc_Minutes;
+        }
     }
-    while(!os_GetCSC());
     gfx_End();
     return 0;
 }
