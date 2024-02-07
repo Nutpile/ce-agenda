08e88e2a210744f950a0def4f44a96463635def6
Nutpile ‎ <Nutpile@hotmail.com>
2024-02-07 21:30:50 +0100

    file read test

--- src/main.c
+++ src/main.c
@@ -23,20 +23,23 @@ SOFTWARE.*/
 #include <sys/rtc.h>
 #include <graphx.h>
 #include <ti/getcsc.h>
+#include <fileioc.h>
 
-void drawstatusbar()
+void drawstatusbar(void)
 {
     uint8_t day = 0;
     uint8_t month = 0;
     uint16_t year = 0;
-    gfx_SetTextXY(5, 4);
+    gfx_SetTextXY(5, 8);
     gfx_SetColor(195);
-    gfx_FillRectangle_NoClip(0, 0, 320, 15);
+    gfx_FillRectangle_NoClip(0, 0, 320, 22);
+    gfx_SetColor(255);
+    gfx_Rectangle_NoClip(1, 1, 318, 20);
     if(!rtc_IsBusy()){
         gfx_PrintUInt(rtc_Hours, 2);
         gfx_PrintChar(*":");
         gfx_PrintUInt(rtc_Minutes, 2);
-        gfx_SetTextXY(235, 4);
+        gfx_SetTextXY(235, 8);
         boot_GetDate(&day, &month, &year);
         gfx_PrintUInt(day, 2);
         gfx_PrintChar(*"/");
@@ -46,9 +49,37 @@ void drawstatusbar()
     }
 }
 
-int main()
+void filedecode(void)
+{
+    char date[17] = "dd/mm/yyyy hh:mm";
+    char msg[33] = "This is a test message, 32 chars";
+    uint8_t AVid = 0;
+    AVid = ti_Open("AgendaAV", "r");
+    if(AVid == 0){
+        gfx_End();
+        exit(1);
+    }
+    ti_Rewind(AVid);
+    ti_Read(date, 17, 1, AVid);
+    ti_Read(msg, 33, 1, AVid);
+    ti_Close(AVid);
+    AVid = 0;
+    //temp code! 
+    gfx_PrintStringXY(date, 4, 40);
+    gfx_PrintStringXY(msg, 4, 50);
+}
+
+int main(void)
 {
     gfx_Begin();
+    //temp code! for testing only, delete later.
+    uint8_t AVid = 0;
+    AVid = ti_Open("AgendaAV", "w");
+    ti_Write(&"09/09/2009 05:27", 17, 1, AVid);
+    ti_Write(&"Hi if you see this msg it worked", 33, 1, AVid);
+    ti_Close(AVid);
+    //end of temp code
+    filedecode();
     uint8_t barupdate = -1;
     while(!os_GetCSC()){
         if (barupdate != rtc_Minutes){
